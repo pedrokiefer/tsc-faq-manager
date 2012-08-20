1fd2cbac8ab1f9f8c0e89df5a3530c15fa47c7b9
Pedro Kiefer <pedro@kiefer.com.br>
2012-08-20 18:58:53 -0300

    Add Tecnosenior Skin

---
+++ skins/tecnosenior/skin.php
@@ -0,0 +1,112 @@
+<?php
+/*
+Skin Name: Tecnosenior FAQ Skin
+Description: Tecnosenior FAQ Skin
+Version: 1.0
+Author: Pedro Kiefer
+Author URI: http://www.pedro.kiefer.com.br
+ */
+
+/**
+ * User: Pedro Kiefer <pedro@tecnosenior.com>
+ * Date: 8/20/12
+ * Time: 10:27 AM
+ */
+
+function tsc_skin_get_headers()
+{
+    $headers = array(
+        "js" => array(
+            "jquery",
+            "jquery-ui",
+            array("name" => "tsc-faq", "file" => plugin_dir_url(__FILE__) . "/tsc-faq.js")
+        ),
+        "css" => array("tsc-faq" => plugin_dir_url(__FILE__) . "/tsc-faq.css")
+    );
+
+    return $headers;
+}
+
+function tsc_skin_render($group, $questions)
+{
+    $html = "";
+    if ($group->SearchBox) {
+        $html .= render_search_box($group->Id);
+    }
+
+    $i = 0;
+    $html .= "<div class=\"span-14 prepend-1 append-1 last faq-questions\" id=\"faq-questions-list\">\n";
+    foreach ($questions as $q) {
+        $html .= render_question($q, ($i % 2 == 1));
+        $i++;
+    }
+    $html .= "</div>";
+
+    if ($group->AskBox) {
+        $html .= render_ask_box($group->Id);
+    }
+
+    return $html;
+}
+
+function render_search_box($groupId)
+{
+    $html = "";
+    $html .= "<div class=\"span-14 prepend-1 append-1 last faq-search-box\">";
+    $html .= "<form action=\"\" name=\"search\" method=\"post\">";
+    $html .= "<input type=\"hidden\" name=\"action\" value=\"faqQuery\"/>";
+    $html .= "<input type=\"hidden\" name=\"req\" value=\"search\"/>";
+    $html .= "<input type=\"hidden\" name=\"groupId\" value=\"{$groupId}\"/>";
+    $html .= "    <label for=\"faq-search\" class=\"faq-search\">Palavra Chave</label>";
+    $html .= "    <div class=\"faq-search-field\">";
+    $html .= "        <input type=\"text\" name=\"faq_search\" id=\"faq-search\" value=\"\"/>";
+    $html .= "        <img src=\"" . plugin_dir_url(__FILE__) . "faq-magnifier.png\" width=\"22\" height=\"28\">";
+    $html .= "    </div>";
+    $html .= "</form>";
+    $html .= "</div>";
+
+    return $html;
+}
+
+function render_question($question, $even)
+{
+    $evenStr = $even ? "even" : "odd";
+
+    $html = "<div class=\"faq-question\">";
+    $html .= "<div class=\"question-head " . $evenStr . "\">";
+    $html .= "    <h2><a href=\"#\">" . $question->Question . "</a></h2>";
+    $html .= "</div>";
+    $html .= "<div class=\"question-body\">";
+    $html .= $question->Answer;
+    $html .= "</div>";
+    $html .= "</div>";
+
+    return $html;
+}
+
+function render_ask_box($groupId)
+{
+
+    $html = "<div class=\"span-14 prepend-1 append-1 last faq-ask-question\">";
+    $html .= "    <h2>Envie sua pergunta!</h2>";
+    $html .= "<form action=\"\" name=\"ask-question\" method=\"post\">";
+    $html .= "<input type=\"hidden\" name=\"action\" value=\"faqQuery\"/>";
+    $html .= "<input type=\"hidden\" name=\"req\" value=\"question\"/>";
+    $html .= "<input type=\"hidden\" name=\"groupId\" value=\"{$groupId}\"/>";
+    $html .= "    <fieldset class=\"faq-form\">";
+    $html .= "        <div>";
+    $html .= "            <label for=\"email-address\">Email</label>";
+    $html .= "            <input type=\"text\" id=\"email-address\" name=\"email-address\"/>";
+    $html .= "        </div>";
+    $html .= "        <div>";
+    $html .= "            <label for=\"new-question\">D&uacute;vida</label>";
+    $html .= "            <textarea id=\"new-question\" name=\"new-question\"></textarea>";
+    $html .= "        </div>";
+    $html .= "        <div>";
+    $html .= "            <input type=\"submit\" id=\"submit\" name=\"submit\" class=\"faq-form-button\" value=\"Enviar\">";
+    $html .= "        </div>";
+    $html .= "    </fieldset>";
+    $html .= "</form>";
+    $html .= "</div>";
+    return $html;
+}
---
+++ skins/tecnosenior/tsc-faq.js
@@ -0,0 +1,68 @@
+/**
+ * Pedro Kiefer <pedro@tecnosenior.com>
+ * Date: 8/20/12
+ */
+
+(function ($) {
+    $.fn.faqSearch = function (options) {
+        var renderResult = function (data, even) {
+            var rowClass = even ? "even" : "odd";
+            var header = $("<div/>", {class: "question-head "+ rowClass})
+                .append($("<h2></h2>")
+                    .append($("<a></a>", {text:data["question"], href:"#"}))
+                )
+
+            header.click(function () {
+                $(this).next().slideToggle('slow');
+                return false;
+            });
+            var content = $("<div></div>", {class:"question-body"}).html(data["answer"]).hide();
+            var html = $("<div></div>", {class:"faq-question"}).append(header).append(content);
+
+            return html;
+        };
+
+        var settings = $.extend({
+            target:false,
+            renderResult:renderResult
+        }, options);
+
+        return this.each(function () {
+            $(this).submit(function (e) {
+                e.preventDefault();
+                var form = $(this);
+                $.post(TscFaqAjax.ajaxurl, form.serialize(), function (data) {
+                    if (data.status == "error") {
+                        log.console(data);
+                    } else if (data.status == "success") {
+                        $(settings.target).empty();
+                        $.each(data.result, function(index, value) {
+                            $(settings.target).append(settings.renderResult(value, (index % 2 == 1)));
+                        })
+                    }
+                });
+            });
+        });
+    }
+})(jQuery);
+
+jQuery(document).ready(function ($) {
+    $("form[name=search]").faqSearch({
+       target: $("#faq-questions-list")
+    });
+    $("form[name='ask-question']").submit(function(e) {
+        e.preventDefault();
+        var form = $(this);
+        $.post(TscFaqAjax.ajaxurl, form.serialize(), function(data) {
+            if(data.status == "error") {
+                alert(data.message);
+            } else if (data.status == "success") {
+                alert("Sent!");
+            }
+        });
+    });
+    $('.faq-questions .question-head').click(function () {
+        $(this).next().slideToggle('slow');
+        return false;
+    }).next().hide();
+});
